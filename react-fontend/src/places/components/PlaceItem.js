034a595f557fa899d25ebcb7c291431eda7bbb64
trannam96 <namit1951996@gmail.com>
2020-04-17 16:39:52 +0700

    update to be learn basic

--- react-fontend/src/places/components/PlaceItem.js
+++ react-fontend/src/places/components/PlaceItem.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./PlaceItem.css";
-import PlaceList from "./PlaceList";
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+
+import Modal from "../../shared/components/UIElements/Modal";
 
 const PlaceItem = (props) => {
-  console.log(props);
+  const [showMap, setShowMap] = useState(false);
+
+  const showMapHandler = () => setShowMap(true);
+
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="place-item">
-      <Card>
-        <div className="place-item__title">
-          <img src={props.imageUrl} alt={props.title} />
-        </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
-        </div>
-      </Card>
-    </li>
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal_content"
+        footerClass="place-item__modal_actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container"></div>
+      </Modal>
+      <li className="place-item">
+        <Card>
+          <div className="place-item__title">
+            <img src={props.imageUrl} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={showMapHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
---
+++ react-fontend/src/shared/components/UIElements/Map.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Map = (props) => {
+  return <div className={`map ${props.className}`} style={props.style}></div>;
+};
+
+export default Map;
